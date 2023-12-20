c0231a45b13e5b0afd91a7002c4bfc69ec9184b0
NewDron <73710895+NewDron@users.noreply.github.com>
2023-12-20 17:08:50 +0200

    Add files via upload

---
+++ Pohikonstruktsioonid.py
@@ -0,0 +1,37 @@
+#print("Tere maailm!")
+#nimi = input("Mis on sinu nimi?")
+#vanus = int(input("Kui vana sa oled?"))
+
+#print("Tere "+nimi+"! Sa oled "+str(vanus)+" aastat vana!")
+#print("Tere ",nimi,"! Sa oled ",vanus," aastat vana!")
+#print("Tere {0}! Sa oled (1) aastat vana!".format(nimi,vanus))
+
+#print(type(nimi))
+#print(type(vanus))
+
+#arv1=float(input("Arv 1:"))
+#t=input("Tehe: ")
+#arv2=float(input("Arv 2:"))
+#vastus=eval(str(arv1)+t+str(arv2))
+#print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+
+#vanus = 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#kas_käib_koolis = True
+
+#print(type(vanus))
+#print(type(eesnimi))
+#print(type(pikkus))
+#print(type(kas_käib_koolis))
+
+#from random import * #koik funktsioonid mis on moodulis
+#kogus=randint(1,100)
+#print("Kokku on",kogus,"kommid")
+#mitu=int(input("Mitu kommi tahad?"))
+#print(kogus-mitu, "Komme jaanud")
+
+from math import *
+l=float(input("Umbermoot:"))
+d=l/pi          #3.14...
+print("Labimoot= ", d)
